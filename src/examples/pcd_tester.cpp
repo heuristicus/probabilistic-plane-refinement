26b170c50ae9910852bef35fb40cf599e909b12e
Michal Staniaszek <m.staniaszek@gmail.com>
2015-03-04 14:53:39 +0100

    Fix package name, add catkin_package to CMakeLists, comment conflicting lib

--- src/examples/pcd_tester.cpp
+++ src/examples/pcd_tester.cpp
@@ -5,7 +5,7 @@
 #include <pcl/io/pcd_io.h>
 
 #include <pcl/io/io.h>
-#include <pcl/features/integral_image_normal.h>
+//#include <pcl/features/integral_image_normal.h>
 #include <pcl/ModelCoefficients.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
